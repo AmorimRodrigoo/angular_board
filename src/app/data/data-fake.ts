4ba6b3ff7d5fa32e626379dfd160bdc7b22298f2
Rodrigo Amorim <amorimrodrigo071@gmail.com>
2025-03-17 01:06:33 -0300

    adding infos to  mock database

--- src/app/data/data-fake.ts
+++ src/app/data/data-fake.ts
@@ -9,21 +9,21 @@ export const dataFake = [
     {
         "id": "2",
         "title": "Tenha Paciência, Sport avança para a final",	
-        "description": "aaa",
+        "description": "O Sport venceu o Santa Cruz por 1 a 0 neste sábado (15), no Estádio José do Rego Maciel, o Arruda, em Recife, pelo jogo de volta da semifinal do Campeonato Pernambucano.Com robusta vantagem construída na ida, com um 2 a 0, clássico das multidões decisivo teve um controle maior do Sport , com mais chances claras.O gol do Sport foi anotado por Fabrício Dominguez, aos 22 minutos do primeiro tempo.Agora o Leão aguarda o classificado do duelo entre Retrô e Maguary, que definirão o segundo finalista neste domingo (16), às 16h (de Brasília), na Arena Pernambuco. O jogo da ida terminou em 2 a 1 para o Azulão.",
         "contentCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgEhC76S4v61WlyVHs4PX4Au6wbDpUuy8tog&s"
     },
     
     {
         "id": "3",
         "title": "Hamilton e Ferrari.",	
-        "description": "aaa",
+        "description": "Protagonizar o que é considerada uma das maiores transferências do esporte poderia ser um peso para qualquer atleta, mas Lewis Hamilton garante que seu recomeço, na Ferrari, nem de longe traz pressão para ele. Na coletiva de imprensa do GP da Austrália nesta quinta-feira, o veterano de 40 anos detalhou como lida com as expectativas sobre sua estreia no time: sem pressão, e trabalhando muito.",
         "contentCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsb976Sg54YH1WpV4aBLWp8nQXJQLymWWRiQ&s"
     },
     
     {
         "id": "4",
         "title": " Conheça a Unrivaled",	
-        "description": "aaa",
+        "description": "A primeira noite de Unrivaled viu sua arena de 870 lugares no MediaPro Center esgotando. Os fãs alinharam -se em chuva leve algumas horas antes das portas se abrirem, aproveitando as ativações dos fãs que a liga havia criado para eles. No interior, o Hall da Fama Steve Nash e o astro do futebol dos EUA, Alex Morgan, Sat Courtside. Os executivos da WNBA foram polvilhados ao longo das arquibancada",
         "contentCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRro-nnWpXvaKK0UW_Pb8tls2CikN9ldglMXA&s"
     }
 ]
